84804236d417db087a5ce0cdc76bbaea1b7de9eb
zhaojunbo <zhaojunbo1992chasing@gmail.com>
2015-04-27 15:56:25 -0400

    second

--- main.lua
+++ main.lua
@@ -53,78 +53,7 @@ for k, v in pairs(params) do
 end
 print(modelname)
 
---local state_train, state_valid, state_test
 model = {}
---local paramx, paramdx
-
-function lstm(i, prev_c, prev_h)
-  local function new_input_sum()
-    local i2h            = nn.Linear(params.rnn_size, params.rnn_size)
-    local h2h            = nn.Linear(params.rnn_size, params.rnn_size)
-    return nn.CAddTable()({i2h(i), h2h(prev_h)})
-  end
-  local in_gate          = nn.Sigmoid()(new_input_sum())
-  local forget_gate      = nn.Sigmoid()(new_input_sum())
-  local in_gate2         = nn.Tanh()(new_input_sum())
-  local next_c           = nn.CAddTable()({
-    nn.CMulTable()({forget_gate, prev_c}),
-    nn.CMulTable()({in_gate,     in_gate2})
-  })
-  local out_gate         = nn.Sigmoid()(new_input_sum())
-  local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
-  return next_c, next_h
-end
-
-function create_network()
-  local x                = nn.Identity()()
-  local y                = nn.Identity()()
-  local prev_s           = nn.Identity()()
-  local i                = {[0] = LookupTable(params.vocab_size,
-                                                    params.rnn_size)(x)}
-  local next_s           = {}
-  local split         = {prev_s:split(2 * params.layers)}
-  for layer_idx = 1, params.layers do
-    local prev_c         = split[2 * layer_idx - 1]
-    local prev_h         = split[2 * layer_idx]
-    local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
-    local next_c, next_h = lstm(dropped, prev_c, prev_h)
-    table.insert(next_s, next_c)
-    table.insert(next_s, next_h)
-    i[layer_idx] = next_h
-  end
-  local h2y              = nn.Linear(params.rnn_size, params.vocab_size)
-  local dropped          = nn.Dropout(params.dropout)(i[params.layers])
-  local pred             = nn.LogSoftMax()(h2y(dropped))
-  local err              = nn.ClassNLLCriterion()({pred, y})
-  local output           = nn.Identity()(pred)
-  local module           = nn.gModule({x, y, prev_s},
-                                      {err, nn.Identity()(next_s), output})
-  module:getParameters():uniform(-params.init_weight, params.init_weight)
-  return transfer_data(module)
-end
-
-function setup()
-  print("Creating a RNN LSTM network.")
-  local core_network = create_network()
-  paramx, paramdx = core_network:getParameters()
-  model.s = {}
-  model.ds = {}
-  model.start_s = {}
-  for j = 0, params.seq_length do
-    model.s[j] = {}
-    for d = 1, 2 * params.layers do
-      model.s[j][d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
-    end
-  end
-  for d = 1, 2 * params.layers do
-    model.start_s[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
-    model.ds[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
-  end
-  model.core_network = core_network
-  model.rnns = g_cloneManyTimes(core_network, params.seq_length)
-  model.norm_dw = 0
-  model.err = transfer_data(torch.zeros(params.seq_length))
-end
 
 function reset_state(state)
   state.pos = 1
@@ -240,7 +169,6 @@ local states = {state_train, state_valid}
 for _, state in pairs(states) do
  reset_state(state)
 end
-setup()
 step = 0
 epoch = 0
 total_cases = 0
